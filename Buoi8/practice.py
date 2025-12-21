480aa4923737140e448db3d8df0079a6dac7add4
Ngoc Phu <nphu50@gmail.com>
2025-12-21 09:22:33 +0700

    Edit file practice.py

--- Buoi8/practice.py
+++ Buoi8/practice.py
@@ -1,5 +1,23 @@
 class Student:
     def __init__(self, name, score):
-        self.balance = balance
-        self._balance = balance
-        self.__balance = balance
+        self.name = name
+        self.score = score
+
+    def display_info(self):
+        return f"Tên: {self.name}, điểm: {self.score}"
+
+    def is_passed(self):
+        if self.score >= 5:
+            return True
+        else:
+            return False
+
+
+student_obj = Student("Phú", 10)
+student_obj1 = Student("John", 2)
+
+print(student_obj.display_info())
+print(student_obj.is_passed())
+
+print(student_obj1.display_info())
+print(student_obj1.is_passed())
