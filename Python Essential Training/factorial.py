f443fd9f37929d3a19dff80732f151ee7c476757
Ngoc Phu <nphu50@gmail.com>
2025-12-26 16:23:21 +0700

    add new file factorial.py

--- Python Essential Training/factorial.py
+++ Python Essential Training/factorial.py
@@ -1,1 +1,15 @@
-print("hello")
+def factorial(num):
+    if type(num) is str:
+        return None
+    elif num < 0:
+        return None
+    elif type(num) is float:
+        return None
+
+    fact = 1
+    for i in range(1, num + 1):
+        fact = fact * i
+    return fact
+
+
+print(factorial("aas"))
